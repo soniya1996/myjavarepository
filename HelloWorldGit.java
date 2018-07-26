ee54edc6ea4cc96d537462b245e8488e8eb4dd9a
soniya yadav <soniya_yadav@persistent.co.in>
2018-07-26 15:38:21 +0530

    'git checkout -b ideagit checkout -b idea' added and date display

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -3,14 +3,16 @@ class HelloWorld
 
 
 void mymethod(){
-  System.out.ptintln("display date");
+  System.out.ptintln("display date"+new date());
 }
 public static void main(String[] args)
 {
 
 
 
-system.out.println("hii...");
+system.out.println("hii...!!!!");
+
+fhjftgkkkgmgj
 
 }
 }
